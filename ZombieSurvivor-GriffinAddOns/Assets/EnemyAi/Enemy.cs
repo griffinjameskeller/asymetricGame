13b5f3cc931a8d737e4352901555deca7bbda1b4
Luis Gerardo Huiza Blanco <LGHUIZA@luiss-mbp.wireless.newschool.edu>
2016-11-01 12:38:31 -0400

    Added file with Enemy AI

---
+++ ZombieSurvivor-GriffinAddOns/Assets/EnemyAi/Enemy.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	
+	public float deathDistance = 0.5f;
+	public float distanceAway;
+	public bool inSight, active;
+	public Transform enemy, target, routeEnd;
+	public string targetName;
+	private NavMeshAgent navComponent;
+
+	void Start() 
+	{
+		navComponent = this.gameObject.GetComponent<NavMeshAgent>();
+	}
+
+	void FixedUpdate() {
+
+		if (!inSight) {
+			Patrol ();
+		} else if (inSight) {
+			Chase ();
+		}
+
+	}
+
+	void Chase () {
+
+		active = true;
+
+		distanceAway = Vector3.Distance(target.position, transform.position);
+
+		if (routeEnd) {
+			navComponent.SetDestination (target.position);
+		} else if (routeEnd == null) {
+			routeEnd = this.gameObject.GetComponent<Transform> ();
+		} else {
+			routeEnd = GameObject.FindGameObjectWithTag (targetName).transform;
+		}
+
+	}
+
+	void Patrol () {
+
+		distanceAway = Vector3.Distance(routeEnd.position, transform.position);
+
+		if (routeEnd) {
+			navComponent.SetDestination (routeEnd.position);
+		} else if (routeEnd == null) {
+			routeEnd = this.gameObject.GetComponent<Transform> ();
+		} else {
+			routeEnd = GameObject.FindGameObjectWithTag ("Goal").transform;
+		}
+
+	}
+
+	void OnTriggerStay (Collider col) {
+
+		if (col.gameObject.CompareTag ("Player")) {
+			inSight = true;
+			target = col.gameObject.transform;
+			targetName = col.gameObject.name;
+		}
+
+	}
+
+	void OnTriggerExit (Collider col) {
+
+		if (col.gameObject.CompareTag ("Player")) {
+			inSight = false;
+		}
+
+	}
+}
---
+++ ZombieSurvivor-GriffinAddOns/Assets/EnemyAi/Trap.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trap : MonoBehaviour {
+
+	public float proximity;
+	public int uses;
+
+	void Update() {
+
+		if (uses == 0) {
+
+			Destroy (gameObject);
+
+		}
+
+	}
+
+	void OnTriggerStay (Collider col) {
+
+		if (col.gameObject.CompareTag ("Minion")) {
+
+			float distanceAway = Vector3.Distance(col.transform.position, transform.position);
+
+			print (distanceAway);
+
+			if (distanceAway < proximity) {
+
+				uses--;
+				Destroy (col.gameObject);
+
+			}
+
+		}
+
+	}
+}
